c992fea48ba8b97897ff65e5fb8a8bbbff630a5a
Bob Mathews <shibijothy@gmail.com>
2014-12-25 17:35:06 +0530

    Page 132

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141222075905) do
+ActiveRecord::Schema.define(version: 20141225045448) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141222075905) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity",   default: 1
   end
 
   add_index "line_items", ["cart_id"], name: "index_line_items_on_cart_id"
@@ -32,7 +33,7 @@ ActiveRecord::Schema.define(version: 20141222075905) do
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price"
+    t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end

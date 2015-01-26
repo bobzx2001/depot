1c21be022dde2530f457b7bf226e44a4ae7bc65e
Bob Mathews <bob2001@gmail.com>
2015-01-26 21:53:21 +0530

    Page 76, success!

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -61,6 +61,16 @@ class ProductsController < ApplicationController
     end
   end
 
+  def who_bought
+      @product = Product.find(params[:id])
+      @latest_order = @product.orders.order(:updated_at).last
+      if stale?(@latest_order)
+        respond_to do |format|
+          format.atom
+        end
+      end
+    end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
@@ -72,14 +82,5 @@ class ProductsController < ApplicationController
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
 
-    def who_bought
-      @product = Product.find(params[:id])
-      @latest_order = @product.orders.order(:updated_at).last
-      if stale?(@latest_order)
-        respond_to do |format|
-          format.atom
-        end
-      end
-    end
-
+    
 end

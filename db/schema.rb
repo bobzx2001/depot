50c696816c422d1c626c91dae7ce20c45dcca01d
Bob Mathews <shibijothy@gmail.com>
2015-02-17 03:27:43 +0530

    Page 182

--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,7 @@ ActiveRecord::Schema.define(version: 20150107194720) do
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price"
+    t.decimal  "price",       precision: 8, scale: 2
     t.datetime "created_at"
     t.datetime "updated_at"
   end

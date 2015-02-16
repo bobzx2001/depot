f7f2166a89ded4de6b11835fb4008a57ef07d7b9
Bob Mathews <bob2001@gmail.com>
2015-02-17 04:21:18 +0530

    Page 195

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150107194720) do
+ActiveRecord::Schema.define(version: 20150216223341) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -44,7 +44,14 @@ ActiveRecord::Schema.define(version: 20150107194720) do
     t.string   "title"
     t.text     "description"
     t.string   "image_url"
-    t.decimal  "price",       precision: 8, scale: 2
+    t.decimal  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
